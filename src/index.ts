f2251bb60d6f1fc88f780adf5eeced66e7e04804
Brian Lai <a1740942002@me.com>
2025-03-20 01:25:52 +0800

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { server } from './mcp-server'
 // Start the server
 async function main() {
   const transport = new StdioServerTransport()
+
+  // 他會攔截所有 stdout，所以你的 code 不能使用任何 console.log
+  // 否則會被輸出為 MCP 的 response
   await server.connect(transport)
   console.error('Weather MCP Server running on stdio')
 }
